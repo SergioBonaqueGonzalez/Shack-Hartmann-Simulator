49e93ffda905526916e146cf042033b43894c390
SergioBonaqueGonzalez <sergio.bonaque@um.es>
2017-03-01 13:38:03 +0000

    Update convolucion.m

--- convolucion.m
+++ convolucion.m
@@ -1,14 +1,21 @@
 function [convolucion]=convolucion(PSF)
+%Created by Sergio Bonaque-Gonzalez. Optical Engineer.
+%   sergio.bonaque@um.es
+% This function convolves a PSF with and extended object. 
 
-%Abro el objeto
-%[fname,pname] = uigetfile('*.*','Abrir un archivo con el objeto');
+%INPUTS: 
+%PSF: the point spread function.
+
+%OUTPUTS:
+%Convolucion: the convolution of the PSF with the object.
+
+%Open an image
 myExtract=imread('wooptix.tif');
 objeto = myExtract(:,:,1);
 
-%igualo tamaño de matriz
+%Resize matrix size
 [p1, p2] = size(PSF);
 [o1,o2]=size(objeto);
-
 if  rem((p1-o1),2)==0
     objeto2=padarray(objeto,[((p1-o1)/2),((p2-o2)/2)]);
 else
@@ -19,10 +26,10 @@ else
 end
 
 
-%Hago el producto en el espacio de frecuencias
+%Make the product in the frequencies space
 product =fftshift(fft2(double(objeto2))) .* fftshift(fft2(double(PSF)));
 
-%Vuelvo al espacio imagen
+%Coming back to the image space.
 convolution = abs(fftshift(ifft2(double(product))));
 clear product;
 
