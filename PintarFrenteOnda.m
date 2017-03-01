400957c1c6e7c29b420fbd60711445b0ab475d16
SergioBonaqueGonzalez <sergio.bonaque@um.es>
2017-03-01 13:34:19 +0000

    Update PintarFrenteOnda.m

--- PintarFrenteOnda.m
+++ PintarFrenteOnda.m
@@ -1,10 +1,13 @@
 function [pupilpintar]=PintarFrenteOnda(rho,W,pintar)
+%Created by Sergio Bonaque-Gonzalez. Optical Engineer.
+%   sergio.bonaque@um.es
+% This function paint a wavefront inside a pupil and return a pupil with NaN instead of zeros, useful for cosmetic purposes.
 
 pupilpintar=ones(size(rho));
 [a,b]=size(rho); 
 for i=(1:a);
     for j=(1:b);
-        if rho(i,j) > 1 ;%Este es el valor del radio del circulo unidad donde van a estar definidos los zernikes.
+        if rho(i,j) > 1 ;
             pupilpintar(i,j)=NaN;
         end;
     end;
@@ -15,7 +18,7 @@ if pintar==1
     figure;
     subplot(1,3,1)
     imshow(W.*pupilpintar,[])
-    title('Frente de onda original(m)') 
+    title('Original wavefront (m)') 
     colorbar
     set(gcf,'color','w');
     drawnow();
