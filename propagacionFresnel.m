5b78070cd726120ace622e8c6d519cc2d71f0d66
SergioBonaqueGonzalez <sergio.bonaque@um.es>
2017-03-01 14:02:42 +0000

    Update propagacionFresnel.m

--- propagacionFresnel.m
+++ propagacionFresnel.m
@@ -1,27 +1,36 @@
 function[u2]=propagacionFresnel(u1,L,lambda,z,contador)
-% propagacion de Fresnel - La funcion de transferencia asume x e y de la
-% misma longitud y muestreadas uniformemente.
-% u1 - Plano de la fuente
-% L - Cuanto mide el lado del plano de observacion y el plano de la fuente
-% lambda - longitud de onda en metros
-% z - distancia de propagacion
-% u2 - plano de observacion
-[M,~]=size(u1); %Encuentra el tamaño de la imagen de entrada
-dx=L/M; %Intervalo de muestreo
+%Created by Sergio Bonaque-Gonzalez. Optical Engineer.
+%   sergio.bonaque@um.es
+% This function calculates the propagation in the Fresnel regime. The transfer function assumes an uniform sampling. 
+
+
+%INPUTS:
+% u1 - Origin field
+% L1 - Length of the side of the source 
+% lambda - wavelength in meters
+% z - propagation distance in meters. 
+%contador= dummy variable which indicates if a message should be showed or not
+
+%OUTPUTS:
+% L2 - Length of the side of the observed field.  
+% u2 - observed field
+
+[M,~]=size(u1); 
+dx=L/M; 
 crit = abs(lambda*z/L);
-%disp(['dx = ', num2str(dx), ', lambda*z/L = ',num2str(crit)]);
-if contador==0; %Para que solo lo muestre la primera vez;
+
+if contador==0; %It is only showed the first time
     if dx > crit
-        disp('Propagacion en regimen de Fresnel OK!');
+        disp('Propagation in Fresnel regime was succesful!');
     else
-        disp('Muestreo insuficiente');
+        disp('Insufficient sampling');
     end
 end
 
 fx=-1/(2*dx):1/L:1/(2*dx)-1/L; %freq coords 
 [FX,FY]=meshgrid(fx,fx); 
 
-H=exp(-1i*pi*lambda*z*(FX.^2+FY.^2)); %trans func.  The exp(jkz) term is ignored. This term doesn’t affect the transverse spatial structure of the observation plane result.
+H=exp(-1i*pi*lambda*z*(FX.^2+FY.^2)); %trans func.  The exp(jkz) term is ignored. This term doesnâ€™t affect the transverse spatial structure of the observation plane result.
 H=fftshift(H); %shift trans func 
 U1=fft2(fftshift(u1)); %shift, fft src field 
 U2=H.*U1; %multiply 
