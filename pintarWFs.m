73b4f68ca0178b5bc83fa9082df71af4c7600bd3
SergioBonaqueGonzalez <sergio.bonaque@um.es>
2017-03-01 13:55:45 +0000

    Update pintarWFs.m

--- pintarWFs.m
+++ pintarWFs.m
@@ -1,4 +1,7 @@
 function pintarWFs(W,Wrec,pupilpintar)
+%Created by Sergio Bonaque-Gonzalez. Optical Engineer.
+%   sergio.bonaque@um.es
+% This function paints the wavefront.
 
 maximo=max(max(max(W)),max(max(Wrec)));
 maximo2=max(maximo,max(max(W-Wrec)));
@@ -7,7 +10,7 @@ minimo2=min(minimo,min(min(W-Wrec)));
 
 figure
 set(gcf,'color','w');
-suptitle('Fases todas con la misma escala') 
+suptitle('Phases. All with the same scale') 
 subplot(2,2,1);
 imshow(W.*pupilpintar,[])
 caxis manual
@@ -26,11 +29,11 @@ subplot(2,2,3);
 imshow((W-Wrec).*pupilpintar,[])
 caxis manual
 caxis([minimo2 maximo2]);
-title('ERROR (Fase Original - fase recuperada)')
+title('ERROR (Original phase minus recovered phase)')
 colorbar
 
 subplot(2,2,4);
 imshow((W-Wrec).*pupilpintar,[])
-title('ERROR (Fase Original - fase recuperada) AUTO-ESCALADA')
+title('ERROR (Original phase minus recovered phase) AUTO-SCALED')
 colorbar
 drawnow();
