90842bde5cb58d2258742c5942edc563735ead20
SergioBonaqueGonzalez <sergio.bonaque@um.es>
2017-03-01 13:42:35 +0000

    Update mascaraCircular.m

--- mascaraCircular.m
+++ mascaraCircular.m
@@ -1,8 +1,11 @@
 function pupil = mascaraCircular(radio,resolucion)
-%Creacion pupila circular inicial para poder definir zernikes dentro
+%Created by Sergio Bonaque-Gonzalez. Optical Engineer.
+%   sergio.bonaque@um.es
+% Another function that creates a circular mask.
+
 xp=linspace(-1,1,resolucion);
-[X,Y]=meshgrid (xp,xp); %Meshgrid crea una matriz cuyas filas son copias del vector xp, y cuyas columnas son copias del vector xp
-[rho]=sqrt(X.^2+Y.^2); %hipotenusa. Matriz que sustituye cada valor por el de su hipotenusa con respecto a su posicion
+[X,Y]=meshgrid (xp,xp); 
+[rho]=sqrt(X.^2+Y.^2); 
 
 
 pupil=ones(size(rho));
