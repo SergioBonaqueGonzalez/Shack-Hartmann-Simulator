f49af6c2ac1f6e18ef40f7a9bbb4cee31ca2ef46
Sergio Bonaque-Gonzalez <sergio.bonaque@um.es>
2018-02-07 09:01:35 +0000

    Update convolution.m
    Solved a problem in convolution when the resolution is low

--- convolution.m
+++ convolution.m
@@ -17,22 +17,37 @@ objeto = myExtract(:,:,1);
 %Resize matrix size
 [p1, p2] = size(PSF);
 [o1,o2]=size(objeto);
-if  rem((p1-o1),2)==0
-    objeto2=padarray(objeto,[((p1-o1)/2),((p2-o2)/2)]);
-else
-    objeto(:,end)=[];
-    objeto(end,:)=[];
-    [o1,o2]=size(objeto);
-    objeto2=padarray(objeto,[((p1-o1)/2),((p2-o2)/2)]);
+if p1>o1
+    if  rem((p1-o1),2)==0
+        objeto=padarray(objeto,[((p1-o1)/2),((p2-o2)/2)]);
+    else
+        objeto(:,end)=[];
+        objeto(end,:)=[];
+        [o1,o2]=size(objeto);
+        objeto=padarray(objeto,[((p1-o1)/2),((p2-o2)/2)]);
+    end
+elseif p1<o1
+    if  rem((o1-p1),2)==0
+        objeto(end-((o1-p1)/2)+1:end,:)=[];
+        objeto(:,end-((o1-p1)/2)+1:end)=[];
+        objeto(:,1:((o1-p1)/2))=[];
+        objeto(1:((o1-p1)/2),:)=[];
+    else
+        objeto(:,end)=[];
+        objeto(end,:)=[];
+        [o1,o2]=size(objeto);
+        objeto(end-((o1-p1)/2)+1:end,:)=[];
+        objeto(:,end-((o1-p1)/2)+1:end)=[];
+        objeto(:,1:((o1-p1)/2))=[];
+        objeto(1:((o1-p1)/2),:)=[];
+    end
 end
 
-
 %Make the product in the frequencies space
-product =fftshift(fft2(double(objeto2))) .* fftshift(fft2(double(PSF)));
+product =fftshift(fft2(double(objeto))) .* fftshift(fft2(double(PSF)));
 
 %Coming back to the image space.
 convolution = abs(fftshift(ifft2(double(product))));
 clear product;
 
 convolution = convolution./max(max(convolution));
-
